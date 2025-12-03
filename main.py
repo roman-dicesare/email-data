e9f445a22d2ef731c95b5cbd1719c0a5003626e8
roman-dicesare <romandicesare1@gmail.com>
2025-12-03 10:53:12 -0800

    updated main. and created csv folder.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import sys
+import csv
 
 
 def main():
     if len(sys.argv) < 2:
         print("Usage: Python3 main.py <file_path>")
         sys.exit(1)
-    else:
-        user_input = sys.argv[1]
-        print(user_input)
-        sys.exit(0)
+
+    with open('csv-files/test.csv') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for row in csv_reader:
+            print(','.join(row))
 
 
 main()
