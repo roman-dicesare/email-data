4486ed9b4936e69e082c8e786c58bf13eef6e9ac
roman-dicesare <romandicesare1@gmail.com>
2025-12-03 13:02:15 -0800

    update main and .gitignore

--- main.py
+++ main.py
@@ -4,13 +4,37 @@ import csv
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: Python3 main.py <file_path>")
+        print("Usage: Python3 main.py 'csv-files/<filename>")
         sys.exit(1)
+    
+    file_name: str = sys.argv[1]
+    
+    try:
+        opened_file = read_email_data(file_name)
+    except FileNotFoundError:
+        print("Error: That file doesn't exist. Try command again.")
+        sys.exit(1)
+    
+    print(f"{file_name} is real and working.")
 
-    with open('csv-files/test.csv') as csvfile:
-        csv_reader = csv.reader(csvfile)
-        for row in csv_reader:
-            print(','.join(row))
+    print(opened_file)
+
+# we want to use CLI to tell it which file to open, and then what to do with that file.
+
+    # open 'test.csv'
+    # read something out of it
+    # modulate that data it read
+    # report back something (in this case test data)
+
+    # want to be able to search by show (production)
+
+def read_email_data(file_name):
+    with open(file_name) as csvfile:
+        reader = csv.DictReader(csvfile)
+        rows = []
+        for row in reader:
+            rows.append(row)
+        return rows
 
 
 main()
