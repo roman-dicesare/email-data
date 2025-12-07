a1445868b8e8533c139dd170c08915d53faed905
roman-dicesare <romandicesare1@gmail.com>
2025-12-07 14:55:24 -0800

    new updates to main and data.py to pull best performing engagement rate from list.

--- data.py
+++ data.py
@@ -1,23 +1,15 @@
 from datetime import datetime
 import sys, csv
 
-
-
-
 def read_email_data(file_name):
     with open(file_name) as csvfile:
         reader = csv.DictReader(csvfile)
         rows = []
         for row in reader:
             rows.append(row)
-        return rows
-    
+        return rows    
 
 def normalize_DateSendStarted(raw_data):
-    print("TYPE:", type(raw_data))
-    print("VALUE:", repr(raw_data))
-    print()
-
     if raw_data is None:
         sys.exit(1)
     
@@ -32,10 +24,42 @@ def normalize_DateSendStarted(raw_data):
     return dt
 
 def get_datetime(dataset, row_number):
-    first_row = dataset[row_number]
-    raw_date = first_row["DateSendStarted"]
+    data_row = dataset[row_number]
+    raw_date = data_row["DateSendStarted"]
     normalized_date : str = normalize_DateSendStarted(raw_date)
     return normalized_date
 
 def get_campaign(dataset, row_number):
-    pass
+    data_row = dataset[row_number]
+    campaign_name = data_row["Campaign"]
+    return campaign_name
+
+def get_totalOpens(dataset, row_number):
+    data_row = dataset[row_number]
+    totalOpens = data_row["TotalOpens"]
+    return totalOpens
+
+def get_engagementRate(dataset, row_number):
+    data_row = dataset[row_number]
+    engagementRate : float = data_row["EngagementRate"]
+    return engagementRate
+
+def get_total_rows_in_data(dataset):
+    total_data_rows = 0 #this assumes the first row is headers
+    for row in dataset:
+        total_data_rows += 1
+    return total_data_rows
+
+def get_best_performer(dataset):
+    total_rows_in_set = get_total_rows_in_data(dataset)
+    best_performer = dataset[1]["EngagementRate"]
+    best_performer_campaign = ""
+
+    for i in range(1, total_rows_in_set):
+        data_row = dataset[i]
+        data_in_row_engagement = data_row["EngagementRate"]
+        data_in_row_campaign = data_row["Campaign"]
+        if data_in_row_engagement > best_performer:
+            best_performer = data_in_row_engagement
+            best_performer_campaign = data_in_row_campaign
+    return best_performer, best_performer_campaign
--- main.py
+++ main.py
@@ -15,6 +15,19 @@ def main():
         print("Error: That file doesn't exist. Try command again.")
         sys.exit(1)
 
-    print(data.get_datetime(dataset, 6)) #how can we eliminate magic numbers here?  
+    print(f"{file_name} loaded succesfully.")
+    print()
+
+    # print(data.get_datetime(dataset, 6)) #how can we eliminate magic numbers here?  
+    # print(data.get_campaign(dataset, 6))
+    # print(f"Total Opens: {data.get_totalOpens(dataset,6)}")
+
+    # print(f"Engagement Rate: {data.get_engagementRate(dataset,6)}")
+
+    print("------------------------")
+    print()
+    print(f"Best Performing Email Engagment Rate: {data.get_best_performer(dataset)}")
+
+    ##end main()##
 
 main()
